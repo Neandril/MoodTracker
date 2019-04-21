8062e85004b8517f047eb42afe959ec5e8c7359d
Remy DALLAVALLE <remy.dalla@gmail.com>
2019-04-22 00:35:47 +0200

    Implemented VerticalViewPager (buttons are not active yet)

---
+++ app/src/main/java/com/neandril/moodtracker/Adapters/ViewPagerAdapter.java
@@ -0,0 +1,4 @@
+package com.neandril.moodtracker.Adapters;
+
+public class ViewPagerAdapter {
+}
---
+++ app/src/main/java/com/neandril/moodtracker/Fragments/mood_superhappy_fragment.java
@@ -0,0 +1,25 @@
+package com.neandril.moodtracker.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.neandril.moodtracker.R;
+
+public class MoodSuperHappy extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.MoodSuperHappy, container, false);
+
+        return rootView;
+    }
+}
